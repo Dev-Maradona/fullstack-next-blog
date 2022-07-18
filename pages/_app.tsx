a30f78cd67e05c1bd9604fab006e41818c004f1e
Dev-Maradona <maradona.0000@yahoo.com>
2022-07-17 23:06:20 -0700

    ..

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,27 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }: AppProps) {
+import { withTRPC } from '@trpc/next';
+import { AppType } from 'next/dist/shared/lib/utils';
+import { AppRouter } from './api/trpc/[trpc]';
+
+const MyApp: AppType = ({ Component, pageProps }) => {
   return <Component {...pageProps} />
-}
+};
 
-export default MyApp
+export default withTRPC<AppRouter>({
+  config({ ctx }) {
+    /**
+     * If you want to use SSR, you need to use the server's full URL
+     * @link https://trpc.io/docs/ssr
+     */
+     const url = process.env.VERCEL_URL
+     ? `https://${process.env.VERCEL_URL}/api/trpc`
+     : 'http://localhost:3000/api/trpc';
+
+    return {
+      url
+    }
+  },
+
+  ssr: true
+})(MyApp);
---
+++ server/createRouter.ts
@@ -0,0 +1,7 @@
+import { Context } from "./context";
+
+import * as trpc from '@trpc/server';
+
+export function createRouter() {
+    return trpc.router<Context>()
+}
